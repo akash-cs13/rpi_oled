1e7d6979ef6bdbd57353484d3d91cf13c2b8de1e
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2022-11-29 19:34:44 +0530

    4th

--- rpi_oled/check.py
+++ rpi_oled/check.py
@@ -4,23 +4,23 @@ cmd = int(input("Choose command:"))
 
 if cmd == 1:
     print("Shutting down.....")
-    subprocess.run("sudo shutdown now")
+    subprocess.run("sudo shutdown now", shell=True)
 
 elif cmd == 2:
     print("Rebootig.....")
-    subprocess.run("sudo reboot now")
+    subprocess.run("sudo reboot now", shell=True)
 
 elif cmd == 3:
     wlan = str(subprocess.check_output("ifconfig | grep -o \"wlan0\"", shell=True), 'utf-8')
     if wlan == "wlan0":
         print("Hotspot off.....")
-        subprocess.run("sudo nmcli con down Mark ifname wlan0")
-        subprocess.run("sudo nmcli radio wifi off")
-        subprocess.run("sudo ifconfig wlan0 down")
+        subprocess.run("sudo nmcli con down Mark ifname wlan0", shell=True)
+        subprocess.run("sudo nmcli radio wifi off", shell=True)
+        subprocess.run("sudo ifconfig wlan0 down", shell=True)
         
     else: 
         print("Hotsopt on........")
         subprocess.run("sudo rfkill unblock wifi; sudo rfkill unblock all")
-        subprocess.run("sudo ifconfig wlan0 up")
-        subprocess.run("sudo nmcli radio wifi on")
-        subprocess.run("sudo nmcli con up Mark ifname wlan0")
+        subprocess.run("sudo ifconfig wlan0 up", shell=True)
+        subprocess.run("sudo nmcli radio wifi on", shell=True)
+        subprocess.run("sudo nmcli con up Mark ifname wlan0", shell=True)
