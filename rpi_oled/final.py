96ecfd54184a843cb69d336b98b258721b82fc4c
Akash CS <84382172+akash-cs13@users.noreply.github.com>
2023-01-07 11:17:47 +0530

    hey ooooo

--- rpi_oled/final.py
+++ rpi_oled/final.py
@@ -3,7 +3,7 @@ import time
 import board
 import digitalio
 import subprocess
-from threading import Thread
+import requests
 import json
 import adafruit_ssd1306
 import RPi.GPIO as GPIO
@@ -11,6 +11,7 @@ from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 
 path = os.path.dirname(__file__) + '/'
+http = "http://192.168.0.168:4900"
 #f = open(path + "configure.json")
 #data = json.load(f)
 #f.close()
@@ -56,13 +57,9 @@ def time_page():
     dates = current_time.strftime("%d/%m %a")
     draw.text((35, 52), dates,  font=roboto_light.font_variant(size=12), fill=255)
 
-def stats():
+def stats(CPU, MemUsage, Disk1, Temperature):
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     IP = str(subprocess.check_output("hostname -I | cut -d\' \' -f1 | head --bytes -1", shell=True), 'utf-8')
-    CPU = str(subprocess.check_output("top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'", shell=True), 'utf-8')
-    MemUsage = str(subprocess.check_output("free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2 }'", shell=True), 'utf-8')
-    Disk = str(subprocess.check_output("df -h | awk '$NF==\"/mnt/hdd\"{printf \"%sB/%sB ~ %s\", $3,$2,$5}'", shell=True), 'utf-8')
-    Temperature = str(subprocess.check_output("vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1", shell=True), 'utf-8')
     draw.text((0, 18),    "\ue917",  font=icon_font, fill=255)
     draw.text((16, 18), IP,  font=roboto_light, fill=255)
     draw.text((0, 35),    "\ue92a",  font=icon_font, fill=255)
@@ -71,12 +68,12 @@ def stats():
     draw.text((85, 35), MemUsage,  font=roboto_light.font_variant(size=11), fill=255)
     draw.text((0, 47), "\ue924",  font=icon_font, fill=255)
     draw.text((0, 51), "\ue924",  font=icon_font, fill=255)
-    draw.text((17, 51), Disk,  font=roboto_light.font_variant(size=11), fill=255)
+    draw.text((17, 51), Disk1,  font=roboto_light.font_variant(size=11), fill=255)
 
 def clean():
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-def notification():
+def notification1(Temperature):
     Under_voltage = subprocess.check_output('vcgencmd get_throttled', shell=True)
     txt = str(Under_voltage, 'utf8').strip().split("=")
     if txt[1] == "0x0":
@@ -84,8 +81,7 @@ def notification():
     else:
         draw.text((97, 0), "\ue927",  font=icon_font, fill=255)
 
-    Temp = subprocess.check_output("vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1", shell=True)
-    txt = float(str(Temp, 'utf8')[:-2])
+    txt = float(str(Temperature, 'utf8')[:-2])
     if txt <= 50:
         draw.text((113, 0), "\ue908",  font=icon_font, fill=255)
     elif txt <= 70:
@@ -93,15 +89,55 @@ def notification():
     else:
         draw.text((113, 0), "\ue91f",  font=icon_font, fill=255)
 
+def notification():
+    Under_voltage = subprocess.check_output('vcgencmd get_throttled', shell=True)
+    txt = str(Under_voltage, 'utf8').strip().split("=")
+    if txt[1] == "0x0":
+        pass
+    else:
+        draw.text((113, 0), "\ue927",  font=icon_font, fill=255)
 
 
 
+def pocketbase(i, CPU, MemUsage, Disk1, Temperature):
+    r = requests.patch(http + "/api/collections/settings/records/000000000000001", json={"latest_id": i})
+    id = str(i)
+    data = {"id": id.zfill(15), "cpu_temperature": float(str(Temperature, 'utf8')[:-2]), "cpu_usage": float(str(
+        CPU, 'utf8')), "disk1": str(Disk1, 'utf8'), "disk2": str(Disk2, 'utf8'), "ram_usage": str(MemUsage, 'utf8')}
+    r = requests.post(http + "/api/collections/rpi_stats/records", json=data)
+    
+
+def pbinit():
+    r = requests.get(http + "/api/collections/settings/records")
+    settings = json.loads(r.text)
+    page = settings["items"][0]["page"]
+    latest_id = settings["items"][0]["latest_id"]
+    return (page,latest_id)
 
 
 
-stats()
-notification()    
-oled.image(image)
-oled.show()
-time.sleep(LOOPTIME)
+page , latest_id = pbinit()   
+while True:
+    latest_id = latest_id + 1 
+    CPU = str(subprocess.check_output("top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'", shell=True), 'utf-8')
+    MemUsage = str(subprocess.check_output("free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2 }'", shell=True), 'utf-8')
+    Disk1 = str(subprocess.check_output("df -h | awk '$NF==\"/mnt/hdd\"{printf \"%sB/%sB ~ %s\", $3,$2,$5}'", shell=True), 'utf-8')
+    Disk2 = str(subprocess.check_output("df -h | awk '$NF==\"/\"{printf \"%sB/%sB ~ %s\", $3,$2,$5}'", shell=True), 'utf-8')
+    Temperature = str(subprocess.check_output("vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1", shell=True), 'utf-8')
+
+    if page == 1:
+        stats(CPU, MemUsage, Disk1, Temperature)
+        notification()
+    elif page == 2:
+        time_page()
+        notification1(Temperature)
+    else: 
+        clean()
+
+    pocketbase(latest_id, CPU, MemUsage, Disk1, Temperature) 
+    page , latest_id = pbinit()
+    
+    oled.image(image)
+    oled.show()
+    time.sleep(LOOPTIME)
 
